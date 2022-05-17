78c20d5a46a7b37878d0adeb66ca3dc21862a665
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-05-17 20:33:27 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Container from './components/Container';
 import Content from './components/Content';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <><Header /><Nav /><Container /><Content /></>
+    <><Header /><Nav /><Container /><Content /><Footer /></>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import '../styles/footer.css'
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, vero eaque! Veritatis accusantium ex vel ab dolorum ducimus sit excepturi tenetur animi, id, blanditiis minus amet soluta ratione, aliquid obcaecati?</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
