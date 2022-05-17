d489de19bcc42113acb8d0e72b9d656e32c9ba31
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-05-17 20:31:32 +0530

    Content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Container from './components/Container';
+import Content from './components/Content';
 import './App.css';
 
 function App() {
   return (
-    <><Header /><Nav /><Container /></>
+    <><Header /><Nav /><Container /><Content /></>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import '../styles/content.css';
+
+const Content = () => {
+    return ( 
+        <div className="content">
+        <section>
+            <h1>Header</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quibusdam enim voluptatum aut quae minima cumque voluptate tempora animi. Incidunt eligendi omnis temporibus quos at molestias praesentium, ad voluptatibus velit repellat. Adipisci corporis pariatur fugiat. Alias veniam atque dolorum sequi voluptatem, sint ducimus! Reprehenderit repellat dolore laudantium et illum ab fugiat, eaque facere odit, laborum pariatur blanditiis, itaque minus obcaecati at optio. Eos quo deleniti quisquam excepturi porro fugiat, voluptatem recusandae perspiciatis soluta eveniet? Consequuntur adipisci praesentium sint laudantium alias minus at, inventore molestias quisquam rem dolor facere quis repudiandae officiis. Esse est deleniti consequatur eaque aspernatur fugiat temporibus iure.</p>
+        </section>
+        <section>
+            <h1>Header</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quibusdam enim voluptatum aut quae minima cumque voluptate tempora animi. Incidunt eligendi omnis temporibus quos at molestias praesentium, ad voluptatibus velit repellat. Adipisci corporis pariatur fugiat. Alias veniam atque dolorum sequi voluptatem, sint ducimus! Reprehenderit repellat dolore laudantium et illum ab fugiat, eaque facere odit, laborum pariatur blanditiis, itaque minus obcaecati at optio. Eos quo deleniti quisquam excepturi porro fugiat, voluptatem recusandae perspiciatis soluta eveniet? Consequuntur adipisci praesentium sint laudantium alias minus at, inventore molestias quisquam rem dolor facere quis repudiandae officiis. Esse est deleniti consequatur eaque aspernatur fugiat temporibus iure.</p>
+        </section>
+    </div>
+     );
+}
+ 
+export default Content;
